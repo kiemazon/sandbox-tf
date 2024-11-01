c037267163fbcfba1cedee0f839108e28ba97653
Kieron Concah <kiemazon.industries@gmail.com>
2024-11-01 17:39:41 +0000

    providers.tf and default tags.tf

---
+++ defaultTags.tf
@@ -0,0 +1,5 @@
+  default_tags {
+    tags = {
+      iac_managed = "terraform"
+    }
+  }
--- provider.tf
+++ provider.tf
@@ -1,9 +1,10 @@
 provider "aws" {
   region  = "eu-west-2"
-
-  default_tags {
-    tags = {
-      iac_managed = "terraform"
+    required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
     }
   }
+  required_version = ">= 1.2.0"
 }
